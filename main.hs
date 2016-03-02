42f56df288502c36ad1f4d95943dce83e839d26a
kejace <kejace@gmail.com>
2016-03-02 14:24:43 +0100

    Initial commit

---
+++ main.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+import           Yesod
+
+import Minimalbug
+
+data HelloWorld = HelloWorld
+
+mkYesod "HelloWorld" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod HelloWorld
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|Hello World!|]
+
+main :: IO ()
+main = do
+		runMinimalBug
+		warp 3000 HelloWorld
